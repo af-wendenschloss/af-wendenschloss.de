cd2db94b34ac54e509ab79cf205f15e88c8a034b
Gregor Noczinski <gregor@noczinski.eu>
2017-01-17 22:09:10 +0100

    ssh-add verwenden um Schlüssel bereits vorher hinzuzufügen

--- tools/bin/upload.sh
+++ tools/bin/upload.sh
@@ -16,7 +16,6 @@ echo "${SCP_PUBLIC_KEY}" | fold -w65  >> "${sshDir}/deploy_rsa.pub"
 echo "---- END SSH2 PUBLIC KEY ----" >> "${sshDir}/deploy_rsa.pub"
 chmod 400 "${sshDir}/deploy_rsa.pub"
 
-
 ssh-add "${sshDir}/deploy_rsa"
 ssh -p "${SCP_PORT}" -o "StrictHostKeyChecking=no" "${SCP_USER}@${SCP_HOST}" /bin/true || /bin/true
 lftp -e "mirror -R public/* ${SCP_DIRECTORY};quit" -u "${SCP_USER}," "sftp://${SCP_HOST}"
